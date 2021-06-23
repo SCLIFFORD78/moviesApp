f7500560849d66ca75943b145b9d3589e8cba901
SCLIFFORD78 <faugh01@gmail.com>
2021-06-23 23:29:10 +0100

    Using react-query to manage browser-based cache

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
@@ -8,20 +7,39 @@ import MovieReviewPage from "./pages/movieReviewPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 import UpcomingMoviesPage from "./pages/UpcomingMoviesPage"; // NEW
 import SiteHeader from "./components/siteHeader";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <SiteHeader /> {/* New Header  */}
-      <Switch>
-        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
-        <Route path="/reviews/:id" component={MovieReviewPage} />
-        <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-        <Route path="/movies/:id" component={MoviePage} />
-        <Route exact path="/" component={HomePage} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader /> {/* New Header  */}
+        <Switch>
+          <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+          <Route path="/reviews/:id" component={MovieReviewPage} />
+          <Route
+            exact
+            path="/movies/favorites"
+            component={FavoriteMoviesPage}
+          />
+          <Route path="/movies/:id" component={MoviePage} />
+          <Route exact path="/" component={HomePage} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
